0cd03ce5888b1223dced6448c6ec8f8a25f19144
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-04-11 10:46:05 -0600

    Implemented PokemonController

---
+++ Pokedex/PokemonController.swift
@@ -0,0 +1,42 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by handje on 4/11/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+import Foundation
+
+class PokemonController {
+    
+    static let baseURL = "http://pokeapi.co/api/v2/pokemon/"
+    
+    static func fetchPokemon(for searchTerm: String, completion: @escaping (Pokemon?) -> Void) {
+        
+        guard let url = URL(string: baseURL) else {
+            completion(nil)
+            return
+        }
+        
+        let searchURL = url.appendingPathComponent(searchTerm)
+        
+        NetworkController.performRequest(for: searchURL, httpMethod: .get) { (data, error) in
+            guard let data = data,
+                let responseDataString = String(data: data, encoding: .utf8) else {
+                    NSLog("No data returned from network request")
+                    completion(nil)
+                    return
+            }
+            
+                guard let jsonDictionary = (try? JSONSerialization.jsonObject(with: data,
+                                                                        options: .allowFragments)) as? [String: Any] else {
+                    completion(nil)
+                    return
+            }
+            
+            let pokemon = Pokemon(dictionary: jsonDictionary)
+            completion(pokemon) 
+        }
+    }
+}
