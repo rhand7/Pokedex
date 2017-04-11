52e1434ca39a6d25ec51ea0d98fb6997e6ba879e
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-04-11 10:03:21 -0600

    Setup PokemonViewController

---
+++ Pokedex/PokemonViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by handje on 4/11/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PokemonViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+}
